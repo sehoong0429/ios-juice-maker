736b693d9e490f274d6855e5887e27d201d05f77
sehong <tjsgh0801@naver.com>
2023-01-05 22:53:42 +0900

    feat: fruitstore가 관리하는 과일의 재고를 나타내는 딕셔너리 선언

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,4 +9,15 @@ import Foundation
 // 과일 저장소 타입
 class FruitStore {
     
+    var fruitStock: [Fruit: Int]
+    
+    init() {
+        fruitStock = [
+            .strawberry: 10,
+            .banana: 10,
+            .pineapple: 10,
+            .kiwi: 10,
+            .mango: 10
+        ]
+    }
 }
