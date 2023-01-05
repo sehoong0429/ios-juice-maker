65af99a774961ea3c7c8285854aea0b72200a29c
HarryHyeon <hharry0728@gmail.com>
2023-01-05 22:43:04 +0900

    feat: Fruit 열거형 정의

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,14 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by Harry on 2023/01/05.
+//
+
+enum Fruit {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
